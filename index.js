2601c45090481cb8b176226dbefd52e9b9af7ba1
Anthony Davis <Anthony_Davis89@icloud.comgit config  --global user.email git config  --global user.email Anthony_Davis89@icloud.comgit config  --global user.email Anthony_Davis89@icloud.com>
2021-05-08 10:45:00 -0400

    updating index file

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var connection = mysql.createConnection({
     host: "localhost",
     port: process.env.PORT || 3306,
     user: "root",
-    password: "ilD!vino007",
+    password: "",
     database: "employeeDB"
 });
 
